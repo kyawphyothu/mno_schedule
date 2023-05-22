5c6d669f20979eabc9c82a32392ff726753d2c67
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-05-22 20:33:51 +0630

    redirect page crated

--- src/assets/PublicHolidayFnc.js
+++
@@ -1,5 +0,0 @@
-import { publicHolidays } from "./PublicHoliday";
-
-export function getPublicHolidayByYear(year) {
-	return publicHolidays.filter(publicHoliday => new Date(publicHoliday.start).getFullYear() === year);
-  }
